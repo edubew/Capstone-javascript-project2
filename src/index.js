02b74f175907ae1e6b2a31728be2a38f34cfae63
Edubew <edubew98@gmail.com>
2022-07-07 03:18:36 +0300

    Add image styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './styles/index.css';
 import logo2 from './assets/logo2.jpg';
+import logo3 from './assets/logo3.jpg';
 
 const logoIcon = document.querySelector('#logoIcon');
 logoIcon.src = logo2;
+
+const epImage = document.querySelector('#epImage');
+epImage.src = logo3;
